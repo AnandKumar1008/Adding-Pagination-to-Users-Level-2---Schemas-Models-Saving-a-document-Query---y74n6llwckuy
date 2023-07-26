4f66914ea03761dba5440c773b3a4c34f1082e1b
AnandKumar <anandkumar031199@gmail.com>
2023-07-26 16:13:27 +0530

    make commit

--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const { off } = require('../models/user.js');
-const users   =require("../models/user.js");
-
+const mongoose = require("mongoose");
+const { off } = require("../models/user.js");
+const users = require("../models/user.js");
 
 // Import routes
 
 //Router Middlewares
 app.use(express.json());
 
-
 //default value for limit is 5 and offset is 0
 //This route should return an array of _id of all the element that need to be returned.
 //output id can be in any order.
 
-app.get("/",async function(req,res){
+app.get("/", async function (req, res) {
+  var ids = [];
 
-    var ids = [];
+  //Write your code here
+  //modify the ids array
+  const limit = parseInt(req.query.limit) || 5;
+  const offset = parseInt(req.query.offset) || 0;
 
-    //Write your code here
-    //modify the ids array
+  if (limit > 5) {
+    return res.status(400).send("Maximum limit value is 5.");
+  }
 
-    res.send(ids);
+  const usersInRange = await users
+    .find({}, { _id: 1 })
+    .limit(limit)
+    .skip(limit * offset)
+    .exec();
 
+  ids = usersInRange.map((user) => user._id);
+
+  res.send(ids);
 });
 
 module.exports = app;
-
-
